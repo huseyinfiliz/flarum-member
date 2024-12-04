3d846c547984bf7ccc42b3967f6ccddc0a2cb46c
Hüseyin Filiz <37220000+huseyinfiliz@users.noreply.github.com>
2024-12-04 16:04:13 +0300

    Update index.ts

--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -3,7 +3,7 @@ import { extend } from 'flarum/common/extend';
 import ForumApplication from 'flarum/forum/ForumApplication';
 import Member from './components/Member';
 
-app.initializers.add('zerosonesfun-flarum-member', () => {
+app.initializers.add('huseyinfiliz-flarum-member', () => {
   extend(ForumApplication.prototype, 'mount', () => {
     m.mount(document.body.appendChild, Member);
   });
